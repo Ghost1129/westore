a0cd58647fc38baef494507317d3de45112b5f97
Devansh Lodhi <1129.gh0st@gmail.com>
2022-02-13 10:24:00 +0530

    removed unnecessary hooks

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import styled from "styled-components";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
